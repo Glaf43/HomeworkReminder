3dee16697e17349abc4722909a28466ab07123e0
mr_glaf43 <marknguyen431992@gmail.com>
2015-03-30 22:59:20 -0500

    third iteration

---
+++ app/src/main/java/hdnf39/edu/umkc/homeworkreminder/SecondActivity.java
@@ -0,0 +1,22 @@
+package hdnf39.edu.umkc.homeworkreminder;
+
+/**
+ * Created by mr_glaf43 on 3/30/2015.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+import hdnf39.edu.umkc.homeworkreminder.R;
+
+/**
+ * This is the activity that is started when the user presses the notification in the status bar
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_second);
+    }
+
+}
